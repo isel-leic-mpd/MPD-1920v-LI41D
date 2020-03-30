b6dd5fc621ba226dca5f707477a19137cee18111
Luís Falcão <lfalcao@cc.isel.pt>
2020-03-30 16:00:37 +0100

    Lesson 08: LimitIterator implemented using Optional to keep next value.

--- weather/src/main/java/pt/isel/leic/mpd/v1920/li41d/queries/iterators/LimitIterator.java
+++ weather/src/main/java/pt/isel/leic/mpd/v1920/li41d/queries/iterators/LimitIterator.java
@@ -1,10 +1,15 @@
 package pt.isel.leic.mpd.v1920.li41d.queries.iterators;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 public class LimitIterator<T> implements Iterator<T> {
     private final Iterator<T> srcIterator;
-    private final int n;
+    private int n;
+
+    Optional<T> next = Optional.empty();
+
 
     public LimitIterator(Iterator<T> srcIterator, int n) {
         this.srcIterator = srcIterator;
@@ -13,11 +18,21 @@ public class LimitIterator<T> implements Iterator<T> {
 
     @Override
     public boolean hasNext() {
-        return false;
+        if(next.isPresent()) return true;
+        if(n == 0 || !srcIterator.hasNext()) return false;
+        --n;
+        next = Optional.ofNullable(srcIterator.next());
+        return true;
     }
 
     @Override
     public T next() {
-        return null;
+        if(hasNext()) {
+
+            final T t = next.get();
+            next = Optional.empty();
+            return t;
+        }
+        throw new NoSuchElementException();
     }
 }
